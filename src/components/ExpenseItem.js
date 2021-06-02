879bcd203500d4aef28e07f14601018ba89b3e52
OmBelote <ombelote@gmail.com>
2021-06-02 20:20:21 +0530

    Implementing props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,28 +1,17 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$292.6</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
 }
-
-// function ExpenseItem() {
-//   return (
-//     <div className="expense-item">
-//       <div>March 28th 2021</div>
-//       <div className="expense-item__description">
-//         <h2>Car Insurance</h2>
-//         <div className="expense-item__price">$292.6</div>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default ExpenseItem;

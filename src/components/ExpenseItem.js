d163b75fff73b28e743dab866d79271c1a9c9e29
OmBelote <ombelote@gmail.com>
2021-06-02 16:00:38 +0530

    First-commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$292.6</div>
+      </div>
+    </div>
+  );
+}
+
+// function ExpenseItem() {
+//   return (
+//     <div className="expense-item">
+//       <div>March 28th 2021</div>
+//       <div className="expense-item__description">
+//         <h2>Car Insurance</h2>
+//         <div className="expense-item__price">$292.6</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+export default ExpenseItem;
